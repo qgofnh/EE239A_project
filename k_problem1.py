63bf81ceee7a1c2213c5a45c1f8242ef47312cec
kailunqian <kailunece@gmail.com>
2016-02-12 16:35:23 -0800

    kkk
    kkk

--- k_problem1.py
+++ k_problem1.py
@@ -4,3 +4,4 @@ from sklearn.datasets import fetch_20newsgroups
 categories = ['com.graphics']
 graphics_train = fetch_20newsgroups(subset ='train',categories = categories,shuffle=True, random_state=42 )
 
+hgkhgkghkghghjgkj

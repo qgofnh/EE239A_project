37a7e6ab7ca924d1f78eefeae6f2382982f49368
kailunqian <kailunece@gmail.com>
2016-02-12 16:23:48 -0800

    K_proba
    problem a

---
+++ k_problem1.py
@@ -0,0 +1,6 @@
+# problem 1
+from sklearn.datasets import fetch_20newsgroups
+
+categories = ['com.graphics']
+graphics_train = fetch_20newsgroups(subset ='train',categories = categories,shuffle=True, random_state=42 )
+

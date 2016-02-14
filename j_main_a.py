a00a0318dfd95750d372f524019f6e14a9c453cb
Will_qian <qgofnh@hotmail.com>
2016-02-13 20:28:00 -0800

    rough draft

--- j_main_a.py
+++ j_main_a.py
@@ -2,7 +2,7 @@
 from sklearn.datasets import fetch_20newsgroups
 categories = [ 'comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware',
               'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
-for i in categories:
-    a = [len(fetch_20newsgroups(subset='train', categories=[i], shuffle=True, random_state=42).data) for i in categories]
+
+a = [len(fetch_20newsgroups(subset='train', categories=[i], shuffle=True, random_state=42).data) for i in categories]
 print a
 
--- x_main.py
+++ x_main.py
@@ -4,13 +4,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 categories = [ 'comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware',
               'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
-for i in categories:
-    a = [len(fetch_20newsgroups(subset='train', categories=[i], shuffle=True, random_state=42).data) for i in categories]
+a = [len(fetch_20newsgroups(subset='train', categories=[i], shuffle=True, random_state=42).data) for i in categories]
 
 print "hello"
 
 # print a
 plt.bar(np.arange(len(a)), a, 0.5, align='center', alpha=0.5)
-plt.xticks(np.arange(len(a)),categories)
+plt.xticks(np.arange(len(a)),categories,rotation=20,ha = 'right')
 plt.show()
+plt.xlabel('Categories')
+plt.ylabel('Count')
 
